caec812a9b81e606df3e90fa2a64bde1c5b7c44c
Zahid Abdillah <zahidabdillah@protonmail.com>
2026-03-03 03:25:16 +0700

    chore: add cloudflare adapter to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  site: 'https://cv.dzhuk.com',
+  output: 'static',
+  adapter: cloudflare(),
+  vite: { plugins: [tailwindcss()] },
+  site: 'https://zahidabdillah.dev',
   integrations: [sitemap()]
 });
